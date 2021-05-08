8e7bf807cbc746bb0acfebdb3fddf1069a65cb3f
tomagent <tommymora2003@gmail.com>
2021-05-07 23:53:08 -0500

    Document js code

--- Day1/script.js
+++ Day1/script.js
@@ -1,14 +1,18 @@
+// Get all the classes of panel
 const panels = document.querySelectorAll(".panel");
 
-panels.forEach(panel => {
-    panel.addEventListener("click", () => {
-        removeActiveClasses()
-        panel.classList.add("active")
-    })
-})
-
+// Removes all the clases active
 function removeActiveClasses() {
     panels.forEach(panel => {
-        panel.classList.remove("active")
-    })
-}
+        panel.classList.remove("active");
+    });
+};
+
+// Loop through all the panels, listen for a click, remove the "active" one and add the class "active" to that panel
+panels.forEach(panel => {
+    panel.addEventListener("click", () => {
+        removeActiveClasses();
+        panel.classList.add("active");
+    });
+});
+
